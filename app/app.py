e211cdff5b502220dacc1d06d1ef440ab74ed597
Julienlin <julien.lin@osedea.com>
2023-05-31 20:58:04 -0400

    add fortune endpoint

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 import requests
 import os
 import socket
-
-from flask import Flask, request
+import subprocess
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -10,8 +10,16 @@ app = Flask(__name__)
 def info():
     return f"server hostname: {socket.gethostname()} server ip: {request.host.split(':')[0]} client ip: {request.remote_addr}"
 
+@app.route("/fortune")
+def tell_fortune():
+    fortune = subprocess.run(["fortune"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    return render_template('fortune.html', fortune=fortune)
 
 
 def main():
     app.run(host="0.0.0.0", port="8080", debug=True)
+
+
+
 
